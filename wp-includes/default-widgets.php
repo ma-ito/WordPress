006ba2b45063bb4f859ba575a672e6856e356bc7
ma-ito <ito.masahiro@cij.co.jp>
2013-11-10 22:58:56 +0900

    Recent Postウィジェット変更

--- wp-includes/default-widgets.php
+++ wp-includes/default-widgets.php
@@ -574,7 +574,7 @@ class WP_Widget_Recent_Posts extends WP_Widget {
 		<ul>
 		<?php while ( $r->have_posts() ) : $r->the_post(); ?>
 			<li>
-				<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
+				<a href="<?php the_permalink() ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a> <span>(<?php global $post; echo ( $post->views ) ? $post->views : 0; ?>PV)</span>
 			<?php if ( $show_date ) : ?>
 				<span class="post-date"><?php echo get_the_date(); ?></span>
 			<?php endif; ?>
